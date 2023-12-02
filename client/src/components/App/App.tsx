1b6ffbf25411033f73e09e8a9c4da74c9c60d20b
qscgyjqscgyj <aliestarten@gmail.com>
2023-12-02 17:23:07 +0100

    Added todo list mvp component

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,7 +1,14 @@
+import TodoList from '../TodoList';
+
 import './App.css';
 
 function App() {
-    return <div data-test-id="app_container">React App</div>;
+    return (
+        <>
+            <div data-test-id="app_container">React App</div>
+            <TodoList />
+        </>
+    );
 }
 
 export default App;
